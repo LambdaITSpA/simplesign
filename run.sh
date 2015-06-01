db887a86b5286d176afe672a658ced825f08e997
Claudio Guerra <claudevandort@gmail.com>
2015-06-01 12:51:45 -0300

    add dev.simplesign.cl apache config file

--- run.sh
+++ run.sh
@@ -3,9 +3,11 @@ docker run -p 127.0.0.1:5432:5432 --name simplesign-pg -d lambdait/simplesign-pg
 docker run --net="host" \
 -v $PWD/docker/rails/000-default.conf:/etc/apache2/sites-available/default:ro \
 -v $PWD/docker/rails/simplesign.cl.conf:/etc/apache2/sites-enabled/simplesign.cl:ro \
+-v $PWD/docker/rails/dev.simplesign.cl.conf:/etc/apache2/sites-enabled/dev.simplesign.cl:ro \
 -v $PWD/docker/rails/lambdait.cl.conf:/etc/apache2/sites-enabled/lambdait.cl:ro \
 --add-host simplesign.cl:127.0.0.1 \
 --add-host www.simplesign.cl:127.0.0.1 \
+--add-host dev.simplesign.cl:127.0.0.1 \
 --add-host lambdait.cl:127.0.0.1 \
 --add-host www.lambdait.cl:127.0.0.1 \
 -v $PWD:/home/ubuntu/webapp \
