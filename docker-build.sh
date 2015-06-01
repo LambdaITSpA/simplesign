09360f808c5f848bd6557c35b5cb255df48f3f4e
Claudio Guerra <claudevandort@gmail.com>
2015-05-31 22:05:51 -0300

    improve apache config files handling

--- docker-build.sh
+++ docker-build.sh
@@ -4,4 +4,5 @@ docker build -t lambdait/simplesign-rails docker/rails
 docker build -t lambdait/simplesign-pg docker/pg
 rm docker/rails/Gemfile docker/rails/Gemfile.lock
 #docker run -d -p 127.0.0.1:80:80 --name rails rails
-#docker push patriciojara/firmaexpress-rails:prod
+docker push lambdait/simplesign-rails
+docker push lambdait/simplesign-pg
--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 docker rm -f simplesign-rails simplesign-pg
 docker run -p 127.0.0.1:5432:5432 --name simplesign-pg -d lambdait/simplesign-pg
-docker run --net="host" -v $PWD:/home/ubuntu/webapp --name simplesign-rails -d lambdait/simplesign-rails
+docker run --net="host" \
+-v $PWD/docker/rails/000-default.conf:/etc/apache2/sites-available/default:ro \
+-v $PWD/docker/rails/simplesign.cl.conf:/etc/apache2/sites-enabled/simplesign.cl:ro \
+-v $PWD/docker/rails/lambdait.cl.conf:/etc/apache2/sites-enabled/lambdait.cl:ro \
+--add-host simplesign.cl:127.0.0.1 \
+--add-host www.simplesign.cl:127.0.0.1 \
+--add-host lambdait.cl:127.0.0.1 \
+--add-host www.lambdait.cl:127.0.0.1 \
+-v $PWD:/home/ubuntu/webapp \
+--name simplesign-rails -d lambdait/simplesign-rails
