bfb5d9ce08c014ae89f6dbcd382cef1bc685181b
Claudio Guerra <claudevandort@gmail.com>
2015-06-03 13:57:11 -0300

    set dev env to dev.simplesign.cl

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ docker run --net="host" \
 -v $PWD/docker/rails/simplesign.cl.conf:/etc/apache2/sites-enabled/simplesign.cl:ro \
 -v $PWD/docker/rails/lambdait.cl.conf:/etc/apache2/sites-enabled/lambdait.cl:ro \
 --add-host simplesign.cl:127.0.0.1 \
+--add-host dev.simplesign.cl:127.0.0.1 \
 --add-host www.simplesign.cl:127.0.0.1 \
 --add-host lambdait.cl:127.0.0.1 \
 --add-host www.lambdait.cl:127.0.0.1 \
